f582e75200ee71989c278cf2ef6caba206ca338f
Christopher Robert Philabaum <chaos7theory@live.com>
2016-01-24 00:27:54 -0700

    Fixed POST request.

--- models/path.js
+++ models/path.js
@@ -5,17 +5,17 @@ function Path(color, points) {
 
 Path.prototype.postQuery = function(db) {
     var path = '(';
-    for(var i = 0; i < this.path.length; i++) {
+    for(var i = 0; i < this.points.length; i++) {
         var x = this.points[i].latitude + 90;
         var y = this.points[i].longitude + 180;
-        path += `(${x}, ${y})`
-        if(i < this.path.length - 1) {
+        path += `(${x},${y})`
+        if(i < this.points.length - 1) {
             path += ',';
         }
     }
     path += ')';
     return db.one(`INSERT INTO paths
-        (color, path) VALUES ('$1^', path '$2^')
+        (color, points) VALUES ('$1^', path '$2^')
         RETURNING id;
     `, [ this.color, path ]);
 };
--- routes/paths.js
+++ routes/paths.js
@@ -8,16 +8,15 @@ var Path = require('../models/path');
 /* POST paths listing. */
 router.post('/', function(req, res, next) {
     req.accepts('application/json');
-    console.log(req.body);
     var db = pgp(connObj);
-    var pathJson = JSON.parse(req.body);
-    var path = new Path(pathJson.color, pathJson.points);
+    var path = new Path(req.body.color, req.body.points);
     path.postQuery(db)
         .then(function(data) {
             res.sendStatus(201);
         })
         .catch(function(error) {
             res.sendStatus(500);
+            console.error(error);
         });
 });
 
